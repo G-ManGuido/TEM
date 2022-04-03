8679c04ee666dfd1c9a480a7c3f087d1ab9fa57b
G-ManGuido <supercoder@AirvonSperCoder.fritz.box>
2022-04-03 22:16:50 +0200

    neue IMGs

--- assets/js/jplayer.js
+++ assets/js/jplayer.js
@@ -7,31 +7,31 @@ $(document).ready(function () {
     }, [
 
         {
-            title: "<b>Tim</b> 02:40",
+            title: "<b>Tim</b>                  02:40",
             mp3: "mp3/Tim.mp3"
         },
         {
-            title: "<b>Scheduled</b> 01:57",
+            title: "<b>Scheduled</b>            01:57",
             mp3: "mp3/Scheduled.mp3"
         },
         {
-            title: "<b>Fools' Empires</b> 01:57",
+            title: "<b>Fools' Empires</b>      01:57",
             mp3: "mp3/Fools.mp3"
         },
         {
-            title: "<b>Remains Of The Day</b> 02:39",
+            title: "<b>Remains Of The Day</b>  02:39",
             mp3: "mp3/Remains.mp3"
         },
         {
-            title: "<b>Sustaining Days</b> 01:38",
+            title: "<b>Sustaining Days</b>     01:38",
             mp3: "mp3/Sustainingdays.mp3"
         },
         {
-            title: "<b>Punkrockers Lullaby</b> 01:38",
+            title: "<b>Punkrockers Lullaby</b>  01:38",
             mp3: "mp3/Punkrockers.mp3"
         },
         {
-            title: "<b>Sad about Amanda</b> 02:01",
+            title: "<b>Sad about Amanda</b>     02:01",
             mp3: "mp3/Sadaboutamanda.mp3"
         }
 
