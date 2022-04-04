0e871ac848f81375b4618bed38ed1a001d6dc9af
G-ManGuido <supercoder@AirvonSperCoder.fritz.box>
2022-04-04 10:58:04 +0200

    neue Fonts

--- assets/js/jplayer.js
+++ assets/js/jplayer.js
@@ -7,31 +7,31 @@ $(document).ready(function () {
     }, [
 
         {
-            title: "<b>Tim</b>                  02:40",
+            title: "<b>Tim</b>                  <span>02:40</span>",
             mp3: "mp3/Tim.mp3"
         },
         {
-            title: "<b>Scheduled</b>            01:57",
+            title: "<b>Scheduled</b>           <span>01:57</span>",
             mp3: "mp3/Scheduled.mp3"
         },
         {
-            title: "<b>Fools' Empires</b>      01:57",
+            title: "<b>Fools' Empires</b>      <span>01:57</span>",
             mp3: "mp3/Fools.mp3"
         },
         {
-            title: "<b>Remains Of The Day</b>  02:39",
+            title: "<b>Remains Of The Day</b>  <span>02:39</span>",
             mp3: "mp3/Remains.mp3"
         },
         {
-            title: "<b>Sustaining Days</b>     01:38",
+            title: "<b>Sustaining Days</b>     <span>01:38</span>",
             mp3: "mp3/Sustainingdays.mp3"
         },
         {
-            title: "<b>Punkrockers Lullaby</b>  01:38",
+            title: "<b>Punkrockers Lullaby</b>  <span>01:38</span>",
             mp3: "mp3/Punkrockers.mp3"
         },
         {
-            title: "<b>Sad about Amanda</b>     02:01",
+            title: "<b>Sad about Amanda</b>     <span>02:01</span>",
             mp3: "mp3/Sadaboutamanda.mp3"
         }
 
